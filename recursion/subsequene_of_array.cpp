62fa84b15741cf35dca614f0107f8f1aa68af86c
Kumar123Raju <106899702+Kumar123Raju@users.noreply.github.com>
2023-05-09 16:25:15 +0530

    subsequene_of_array.cpp

---
+++ recursion/subsequene_of_array.cpp
@@ -0,0 +1,25 @@
+class Solution {
+    void subset(vector<int> nums,vector<int> output,int index,vector<vector<int> > &ans){
+            // base condition
+             if(index>=nums.size()){
+                 ans.push_back(output);
+                 return;
+             }
+
+            // exclude
+            subset(nums,output,index+1,ans);
+            // include
+            int element=nums[index];
+            output.push_back(element);
+            subset(nums,output,index+1,ans);       
+
+    }
+public:
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<vector<int> > ans;
+        vector<int> output;
+        subset(nums,output,0,ans);
+        return ans;
+        
+    }
+};
